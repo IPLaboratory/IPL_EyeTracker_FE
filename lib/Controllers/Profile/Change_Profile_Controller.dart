e039e79f94d0d1467531d4f9b25b9158526b0499
207.2 <chohv0724@naver.com>
2024-07-29 16:47:54 +0900

    TASK #81 : 회원 정보 수정 페이지 이미지 변경 시 서버에 전송될 수 있는 코드를 구현하였습니다.

--- lib/Controllers/Profile/Change_Profile_Controller.dart
+++ lib/Controllers/Profile/Change_Profile_Controller.dart
@@ -1,14 +1,51 @@
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:dio/dio.dart' as dio; // dio 패키지 임포트에 별칭 사용
 
 class ProfileController extends GetxController {
   var selectedImage = Rx<XFile?>(null);
-  final ImagePicker picker = ImagePicker();
+  final ImagePicker _picker = ImagePicker();
 
   Future<void> getImage(ImageSource imageSource) async {
-    final XFile? pickedFile = await picker.pickImage(source: imageSource);
-    if (pickedFile != null) {
-      selectedImage.value = XFile(pickedFile.path);
+    final XFile? selectImage = await _picker.pickImage(
+      source: imageSource,
+/*      maxHeight: 75,
+      maxWidth: 75,
+      imageQuality: 60, // 이미지 크기 압축 위해 퀼리티 30으로 낮춤*/
+    );
+
+    if (selectImage != null) {
+      selectedImage.value = selectImage;
+      await patchUserProfileImage(selectImage.path);
+    }
+  }
+
+  Future<void> patchUserProfileImage(String filePath) async {
+    print("프로필 사진을 서버에 업로드 합니다.");
+    var dioClient = dio.Dio();
+    try {
+      dioClient.options.contentType = 'multipart/form-data';
+      dioClient.options.headers = {'token': 'your_token_here'};
+      //'your_token_here' 부분에는 인증 토큰을 넣어야 한다.
+      //서버에 접근할 때 사용되는 인증 정보입니다. 보통은 로그인 시 서버로부터 받은 토큰을 사용
+
+      // dio.MultipartFile 생성
+      final sendData = await dio.MultipartFile.fromFile(filePath, filename: filePath.split('/').last);
+      // dio.FormData 생성
+      final formData = dio.FormData.fromMap({'image': sendData});
+
+      var response = await dioClient.patch(
+        'baseUri/users/profileimage', // 적절한 baseUri와 엔드포인트를 설정하세요
+        //API 서버의 기본 URL이 https://api.example.com이라면:
+        //baseUri는 https://api.example.com
+        // 엔드포인트는 /users/profileimage
+        // '$baseUri/users/profileimage', // baseUri와 엔드포인트를 조합하여 전체 URL
+        data: formData,
+      );
+      print('성공적으로 업로드했습니다');
+      return response.data;
+    } catch (e) {
+      print("이미지 업로드 중 오류 발생: $e");
     }
   }
 }
