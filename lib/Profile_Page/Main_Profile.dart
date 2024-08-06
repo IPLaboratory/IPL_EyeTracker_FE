64a01773d1ae6223e461eb7e458da7a2f189575e
207.2 <chohv0724@naver.com>
2024-08-06 14:36:52 +0900

    TASK #119 : 선택한 프로필로 프로필 수정 페이지가 보이도록 하였습니다.

--- lib/Profile_Page/Main_Profile.dart
+++ lib/Profile_Page/Main_Profile.dart
@@ -6,6 +6,7 @@ import '../Profile_Page/Change_Profile.dart'; // ChangeProfile 파일 임포트
 import '../User_Registration_Page/Widget/User_Registration.dart'; // UserRegistrationPage 파일 임포트
 import '../Controllers/Profile/Main_Profile_Controller.dart'; // MainProfileController 임포트
 import '../Controllers/Profile/Controller_Profile.dart'; // ControllerProfile 임포트
+import '../Color/constants.dart';
 
 class MainProfilePage extends StatelessWidget {
   const MainProfilePage({super.key});
@@ -19,9 +20,9 @@ class MainProfilePage extends StatelessWidget {
           init: ControllerProfile(),
           builder: (profileController) {
             return Scaffold(
-              backgroundColor: const Color(0xFFFFF9D0), // 배경색 설정
+              backgroundColor: AppColors.backgroundColor, // 배경색 설정
               appBar: AppBar(
-                backgroundColor: const Color(0xFFFFF9D0), // 배경색 설정
+                backgroundColor: AppColors.backgroundColor, // 배경색 설정
                 elevation: 0, // 그림자 제거
                 actions: [
                   IconButton(
@@ -46,88 +47,97 @@ class MainProfilePage extends StatelessWidget {
                       text: 'E.T.',
                       fontSize: 50,
                       fontWeight: FontWeight.bold,
-                      color: Colors.black,
+                      color: AppColors.textColor,
                     ),
                     const SizedBox(height: 14),
                     const AdjustableText(
                       text: '아이트랙킹할 프로필을 추가해주세요.',
                       fontSize: 20,
-                      color: Colors.black,
+                      color: AppColors.textColor,
                     ),
-                    Obx(() {
-                      //이미지 표시 여부에 따라 이미지 위젯을 조건부 렌더링
-                      if (controller.isImageVisible.value) {
-                        return Transform.translate(
-                          offset: const Offset(0, 57), // 텍스트 아래로 이동
-                          child: SizedBox(
-                            width: 300, // 원하는 너비로 조절
-                            height: 300, // 원하는 높이로 조절
-                            child: Image.asset(
-                              'assets/Main_Profile.png',
-                              fit: BoxFit.contain,
+                    Expanded(
+                      child: Obx(() {
+                        //이미지 표시 여부에 따라 이미지 위젯을 조건부 렌더링
+                        if (controller.isImageVisible.value) {
+                          return Transform.translate(
+                            offset: const Offset(0, 57), // 텍스트 아래로 이동
+                            child: SizedBox(
+                              width: 300, // 원하는 너비로 조절
+                              height: 300, // 원하는 높이로 조절
+                              child: Image.asset(
+                                'assets/Main_Profile.png',
+                                fit: BoxFit.contain,
+                              ),
                             ),
-                          ),
-                        );
-                      } else {
-                        return Container(
-                          width: double.infinity,
-                          margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                          child: Wrap(
-                            spacing: 20.0, // 자식 요소 간의 수평 간격
-                            runSpacing: 20.0, // 자식 요소 간의 수직 간격
-                            children: List.generate(profileController.profiles.length, (index) {
-                              final profile = profileController.profiles[index];
-                              return GestureDetector(
-                                onTap: () => Get.to(() => UserRegistrationPage()),
-                                child: Container(
-                                  width: 150,
-                                  height: 170,
-                                  child: Column(
-                                    children: <Widget>[
-                                      Container(
-                                        width: 130,
-                                        height: 129,
-                                        decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(20.0),
-                                          image: DecorationImage(
-                                            image: profile.photoPath != null && profile.photoPath!.isNotEmpty
-                                                ? NetworkImage(profile.photoPath!)
-                                                : AssetImage('assets/Default_Profile.jpg') as ImageProvider,
-                                            fit: BoxFit.cover,
+                          );
+                        } else {
+                          return SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                Container(
+                                  width: double.infinity,
+                                  margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                                  child: Wrap(
+                                    spacing: 20.0, // 자식 요소 간의 수평 간격
+                                    runSpacing: 20.0, // 자식 요소 간의 수직 간격
+                                    children: List.generate(profileController.profiles.length, (index) {
+                                      final profile = profileController.profiles[index];
+                                      return GestureDetector(
+                                        onTap: () => Get.to(() => UserRegistrationPage()),
+                                        child: Container(
+                                          width: 150,
+                                          height: 170,
+                                          child: Column(
+                                            children: <Widget>[
+                                              Container(
+                                                width: 130,
+                                                height: 129,
+                                                decoration: BoxDecoration(
+                                                  borderRadius: BorderRadius.circular(20.0),
+                                                  image: DecorationImage(
+                                                    image: profile.photoPath != null && profile.photoPath!.isNotEmpty
+                                                        ? NetworkImage(profile.photoPath!)
+                                                        : const AssetImage('assets/Default_Profile.jpg') as ImageProvider,
+                                                    fit: BoxFit.cover,
+                                                  ),
+                                                ),
+                                                child: controller.isChange.value
+                                                    ? IconButton(
+                                                  onPressed: () {
+                                                    Get.to(() => ChangeProfilePage(profile: profile));
+                                                  },
+                                                  icon: const Icon(Icons.edit, size: 30),
+                                                  color: Colors.white,
+                                                )
+                                                    : Container(), // isChange가 false일 때 빈 컨테이너를 표시
+                                              ),
+                                              Row(
+                                                mainAxisAlignment: MainAxisAlignment.center,
+                                                children: <Widget>[
+                                                  Text(
+                                                    profile.name,
+                                                    textAlign: TextAlign.center,
+                                                    style: const TextStyle(
+                                                      fontFamily: 'Maple', //한글 폰트 적용
+                                                    ),
+                                                  )
+                                                ],
+                                              ),
+                                            ],
                                           ),
                                         ),
-                                        child: controller.isChange.value
-                                            ? IconButton(
-                                          onPressed: () {
-                                            Get.to(() => const ChangeProfilePage());
-                                          },
-                                          icon: const Icon(Icons.edit, size: 30),
-                                          color: Colors.white,
-                                        )
-                                            : Container(), // isChange가 false일 때 빈 컨테이너를 표시
-                                      ),
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: <Widget>[
-                                          Text(
-                                            profile.name,
-                                            textAlign: TextAlign.center,
-                                            style: TextStyle(
-                                              fontFamily: 'Maple', //한글 폰트 적용
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                    ],
+                                      );
+                                    }),
                                   ),
                                 ),
-                              );
-                            }),
-                          ),
-                        );
-                      }
-                    }),
-                    const Spacer(), // 빈 공간을 채워 버튼을 아래로 배치
+                                //프로필 개수가 5개보다 작으면 빈 공간 채우기
+                                if (profileController.profiles.length < 5) const Spacer(),
+                              ],
+                            ),
+                          );
+                        }
+                      }),
+                    ),
                     Padding(
                       padding: const EdgeInsets.all(16.0), // 버튼 주위에 여백 추가
                       child: SizedBox(
