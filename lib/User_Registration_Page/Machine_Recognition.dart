659f17f0ca66ea6ccabb008541797a2dc5c800f3
CHOIZION <czion04@naver.com>
2024-08-09 15:25:43 +0900

    TASK #163 : 등록된 기기 페이지 갱신 안 되는 버그 수정했습니다.

--- lib/User_Registration_Page/Machine_Recognition.dart
+++ lib/User_Registration_Page/Machine_Recognition.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:real_test/Controllers/UserRegistration/Machine_Recogniton_Controller.dart';
+import 'package:real_test/Controllers/UserRegistration/User_Registration_Controller.dart'; // UserRegistrationController import
 import 'package:real_test/Color/constants.dart';
 import 'Already_Device_Part.dart';
-import 'Connected_Device_Part.dart'; // Controller import
-import 'package:real_test/Dismiss_Keyboard.dart'; // DismissKeyboard import
+import 'Connected_Device_Part.dart';
+import 'package:real_test/Dismiss_Keyboard.dart';
 
 class MachineRecognitionPage extends StatefulWidget {
   const MachineRecognitionPage({super.key});
@@ -18,41 +19,47 @@ class _MachineRecognitionPage extends State<MachineRecognitionPage> {
   Widget build(BuildContext context) {
     final MachineRecognitionController controller = Get.put(MachineRecognitionController());
 
-    return Scaffold(
-      backgroundColor: AppColors.backgroundColor, // 배경색 설정
-      appBar: AppBar(
+    return WillPopScope(
+      onWillPop: () async {
+        // UserRegistrationPage로 돌아갈 때 갱신 요청을 보냅니다.
+        Get.find<UserRegistrationController>().fetchAllDevices();
+        return true; // 뒤로 가기 허용
+      },
+      child: Scaffold(
         backgroundColor: AppColors.backgroundColor, // 배경색 설정
-        scrolledUnderElevation: 0,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+        appBar: AppBar(
+          backgroundColor: AppColors.backgroundColor, // 배경색 설정
+          scrolledUnderElevation: 0,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          actions: [
+            const SizedBox(width: 10), // 약간의 간격 추가
+          ],
         ),
-        actions: [
-          // 이전에 'Test_Profile.png'를 사용했던 부분을 제거
-          const SizedBox(width: 10), // 약간의 간격 추가
-        ],
-      ),
-      body: DismissKeyboard(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ConnectedDevicePart(),
-                const Text(
-                  '등록된 기기',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+        body: DismissKeyboard(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ConnectedDevicePart(),
+                  const Text(
+                    '등록된 기기',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                const Divider(color: AppColors.greyLineColor, thickness: 1), // 얇은 회색 선 추가
-                const SizedBox(height: 20),
-                AlreadyDevicePart(),
-              ],
+                  const Divider(color: AppColors.greyLineColor, thickness: 1), // 얇은 회색 선 추가
+                  const SizedBox(height: 20),
+                  AlreadyDevicePart(),
+                ],
+              ),
             ),
           ),
         ),
