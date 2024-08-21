5ca6db54f188383ed6be544044961e35db3c76c0
CHOIZION <czion04@naver.com>
2024-08-21 15:07:08 +0900

    TASK #184 : 제스처 선택 페이지 / 제스처 조회 부분 서버랑 연동했습니다.

--- lib/Feature_Settings_Page/Gesture_Choice.dart
+++ lib/Feature_Settings_Page/Gesture_Choice.dart
@@ -1,3 +1,4 @@
+import 'dart:typed_data'; // Uint8List를 사용하기 위해 추가
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../Controllers/Feature/Gesture_Choice_Controller.dart'; // Gesture_Choice_Controller 경로에 맞게 변경
@@ -7,7 +8,7 @@ class GestureChoice extends StatelessWidget {
 
   final GestureChoiceController controller = Get.put(GestureChoiceController());
 
-  Widget _buildGestureItem(String gestureName, String description, String imagePath) {
+  Widget _buildGestureItem(String gestureName, String description, Uint8List imageData) {
     return Column(
       children: [
         GestureDetector(
@@ -24,8 +25,8 @@ class GestureChoice extends StatelessWidget {
                 padding: const EdgeInsets.symmetric(vertical: 5.0),
                 child: Row(
                   children: [
-                    Image.asset(
-                      imagePath, // 제스처 이미지 경로
+                    Image.memory(
+                      imageData, // 제스처 이미지 바이너리 데이터
                       width: 50,
                       height: 50,
                     ),
@@ -39,7 +40,7 @@ class GestureChoice extends StatelessWidget {
                         ),
                         const SizedBox(height: 5),
                         Text(
-                          '설명: $description',
+                          '$description',
                           style: const TextStyle(fontSize: 14),
                         ),
                       ],
@@ -77,17 +78,24 @@ class GestureChoice extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        _buildGestureItem('북쪽', '무섭다.', 'assets/Gesture_Image.png'),
-        _buildGestureItem('남쪽', '두렵다.', 'assets/Gesture_Image.png'),
-        _buildGestureItem('동쪽', '재밌다.', 'assets/Gesture_Image.png'),
-        _buildGestureItem('서쪽', '즐겁다.', 'assets/Gesture_Image.png'),
-        _buildGestureItem('동남쪽', '행복하다.', 'assets/Gesture_Image.png'),
-        _buildGestureItem('서남쪽', '슬프다.', 'assets/Gesture_Image.png'),
-        _buildGestureItem('동북쪽', '우울하다.', 'assets/Gesture_Image.png'),
-        _buildGestureItem('서북쪽', '화난다.', 'assets/Gesture_Image.png'),
-      ],
-    );
+    return Obx(() {
+      if (controller.gestures.isEmpty) {
+        return const Center(
+          child: Text('No gestures available'),
+        );
+      }
+
+      return ListView.builder(
+        itemCount: controller.gestures.length,
+        itemBuilder: (context, index) {
+          final gesture = controller.gestures[index];
+          return _buildGestureItem(
+            gesture['id'].toString(),
+            gesture['explain'].toString(),
+            gesture['image'] as Uint8List,
+          );
+        },
+      );
+    });
   }
 }
